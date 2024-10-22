d5654f1eb39ce42b552e308cc3b6d75886f4983c
Eden Archambault <eden.archambault@ubisoft.com>
2024-10-22 08:38:00 -0400

    initial

---
+++ NHLBoardsAdsBlocker/ColumnMarker.cs
@@ -0,0 +1,8 @@
+﻿namespace NHLBoardsAdsBlocker
+{
+    internal class ColumnMarker
+    {
+        internal int columnIndex { get; set; }
+        internal int pixelIterator { get; set; }
+    }
+}
---
+++ NHLBoardsAdsBlocker/ImageWithConversionDetails.cs
@@ -0,0 +1,13 @@
+﻿using HPPH;
+
+namespace NHLBoardsAdsBlocker
+{
+    internal class ImageWithConversionDetails
+    {
+        internal byte[] ImagePixels { get; set; }
+        internal IColorFormat ImageColorFormat { get; set; }
+        internal int ImageWidth { get; set; }
+        internal int ImageHeight { get; set; }
+        internal DateTime CreatedAt { get; set; } = DateTime.MinValue;
+    }
+}
